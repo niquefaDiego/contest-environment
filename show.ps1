2d418c8c76ef3da793d9ee50d0c040b8222d5914
Diego Niquefa Velasquez <niquefa.diego@gmail.com>
2018-06-18 19:18:15 -0500

    bug fix

--- show.ps1
+++ show.ps1
@@ -1,12 +1,12 @@
 if ( $args.Count -eq 1 ) {
-    Write-Output "-------------------- INPUT ---------------------"
-    Get-Content "$task\cases\$($args[0]).in"
-    Write-Output "-------------------- ANSWER --------------------"
-    Get-Content "$task\cases\$($args[0]).ans"
-    Write-Output "-------------------- OUTPUT --------------------"
-    Get-Content "$task\cases\$($args[0]).out"
+	Write-Output "-------------------- INPUT ---------------------"
+	Get-Content "$task\cases\$($args[0]).in"
+	Write-Output "-------------------- ANSWER --------------------"
+	Get-Content "$task\cases\$($args[0]).out"
+	Write-Output "-------------------- OUTPUT --------------------"
+	Get-Content "$task\cases\$($args[0]).txt"
 }
 else {
-    Write-Warning "Need the test id as argument"
+	Write-Warning "Need the test id as argument"
 }
 
